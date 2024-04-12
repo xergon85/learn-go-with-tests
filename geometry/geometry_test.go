ed65f8aa888e30d750a2260cc34caa792f5ad424
Christoffer Isenberg <christoffer.isenberg@dohi.se>
2024-04-12 23:16:35 +0200

    Added geometry package

---
+++ geometry/geometry_test.go
@@ -0,0 +1,35 @@
+package geometry
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := rectangle.Perimeter()
+	want := 40.0
+
+	if want != got {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{"Rectangle", Rectangle{12, 6}, 72.0},
+		{"Circle", Circle{10}, 314.1592653589793},
+		{"Triangle", Triangle{6, 6}, 18.0},
+	}
+
+	for _, test := range areaTests {
+		t.Run(test.name, func(t *testing.T) {
+			got := test.shape.Area()
+			if got != test.want {
+				t.Errorf("got %g want %g", got, test.want)
+			}
+		})
+	}
+}
