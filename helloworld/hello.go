897c549166976d5a220fae2422c836b6a8c1a16f
Christoffer Isenberg <christoffer.isenberg@dohi.se>
2024-04-12 12:31:03 +0200

    split hello to its own module

---
+++ helloworld/hello.go
@@ -0,0 +1,31 @@
+package hello
+
+const (
+	french  = "French"
+	spanish = "Spanish"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return prefix
+}
--- main.go
+++ main.go
@@ -1,36 +1,8 @@
 package main
 
-import "fmt"
-
-const (
-	french  = "French"
-	spanish = "Spanish"
-
-	englishHelloPrefix = "Hello, "
-	spanishHelloPrefix = "Hola, "
-	frenchHelloPrefix  = "Bonjour, "
+import (
+	"fmt"
 )
-
-func Hello(name string, language string) string {
-	if name == "" {
-		name = "World"
-	}
-	return greetingPrefix(language) + name
-}
-
-func greetingPrefix(language string) (prefix string) {
-
-	switch language {
-	case spanish:
-		prefix = spanishHelloPrefix
-	case french:
-		prefix = frenchHelloPrefix
-	default:
-		prefix = englishHelloPrefix
-	}
-
-	return prefix
-}
 
 func main() {
 	fmt.Println(Hello("world", ""))
