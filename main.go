e4a9989c1fa1ca435806c58fd31ec04cf62066fe
xergon85 <christoffer.isenberg@gmail.com>
2024-05-15 12:55:06 +0200

    json, embed, routing

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 func main() {
 	//log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(dependency.MyGreetHandler)))
-	// sleeper := mocking.NewConfigurableSleeper(3*time.Second, time.Sleep)
-	// mocking.Countdown(os.Stdout, sleeper)
+	//sleeper := mocking.NewConfigurableSleeper(3*time.Second, time.Sleep)
+	//mocking.Countdown(os.Stdout, sleeper)
 
 }
